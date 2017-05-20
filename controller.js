cbfa1a3bf39ef2337ee0c57ef9fdd9adabf5071b
David Peter Ko <davidpeterko@gmail.com>
2017-05-20 17:27:19 -0500

    first commit, full working bare bones application with wheel builder. needs some error handling for blank fields.

---
+++ controller.js
@@ -0,0 +1,63 @@
+// this will control all the button functions
+
+    function calculate(form) {
+        
+        var constDiameter = parseFloat(form.diameter.value);
+        var oldWidth = parseFloat(form.width.value);
+        var oldOffset = parseFloat(form.offset.value);
+        var oldLipsize = parseFloat(form.lipsize.value);
+        var oldBarrelsize = parseFloat(form.barrelsize.value);
+        var desiredLipsize = parseFloat(form.desired_lipsize.value);
+        var desiredBarrelsize = parseFloat(form.desired_barrelsize.value);
+        var newLipsize;
+        var newBarrelsize;
+        var newWidth;
+        var newOffset;
+        var tempOffset;
+
+        // Debug
+        //alert("the values: " + " " + oldWidth + " " + oldOffset + " " + oldLipsize + " " + oldBarrelsize + " ");
+
+        // Need to check if we get into the offset section
+
+        // Barrel calculation
+        if(oldBarrelsize > desiredBarrelsize)
+        {
+            tempOffset = oldOffset - (12.7 * (oldBarrelsize - desiredBarrelsize));
+        }
+        else if(desiredBarrelsize > oldBarrelsize)
+        {
+            tempOffset = oldOffset + (12.7 * (desiredBarrelsize - oldBarrelsize));
+        }
+        else if(oldBarrelsize == desiredBarrelsize)
+        {
+            tempOffset = oldOffset;
+        }
+
+        // Lip calculation 
+        if(desiredLipsize > oldLipsize)
+        {
+            newOffset = tempOffset - (12.7 * (desiredLipsize - oldLipsize));
+        }
+        else if(oldLipsize > desiredLipsize)
+        {
+            newOffset = tempOffset + (12.7 * (oldLipsize - desiredLipsize));
+        }
+        else if(desiredLipsize == oldLipsize)
+        {
+            newOffset = tempOffset;
+        }
+
+        // New width
+        newWidth = desiredBarrelsize + desiredLipsize;
+        // New lip size;
+        newLipsize = desiredLipsize;
+        newBarrelsize = desiredBarrelsize;
+        
+        document.getElementById("newSpecTitle").innerText = "Your new wheel specs are now: ";
+        document.getElementById("fullSpec").innerText = + constDiameter + "x" + newWidth + " " + parseFloat(newOffset.toFixed(1));
+        document.getElementById("newWidth").innerText = "New width: " + newWidth;
+        document.getElementById("newOffset").innerText = "New offset: " + parseFloat(newOffset.toFixed(1));
+        document.getElementById("newLipsize").innerText = "New lip size: " + newLipsize;
+        document.getElementById("newBarrelsize").innerText = "New barrel size: " + newBarrelsize;
+    }
